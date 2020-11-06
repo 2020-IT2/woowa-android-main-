dbf4a979b0aed1c47bc9ea7ed409ebb34f5bbdaa
Jieunii <yjelbf@naver.com>
2020-11-06 21:34:23 +0900

    Add Market Graph

--- app/src/main/java/com/woowahan/woowahanfoods/Statistic.java
+++ app/src/main/java/com/woowahan/woowahanfoods/Statistic.java
@@ -1,5 +1,6 @@
 package com.woowahan.woowahanfoods;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,13 +10,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
 public class Statistic extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_statistic, container, false);
 
+
         return view;
+
+
     }
 
 }
